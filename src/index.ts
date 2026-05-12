bda9b235b815a5be398d9b02c9bbd87c9e2c86d5
swetzel <seb.o.wetzel@gmail.com>
2026-05-12 19:02:28 -0400

    Function param fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Config, { ConfettiConfig } from "./core/Config";
 import Engine from "./core/Engine";
 
-export default function confetti(c: Partial<ConfettiConfig>): void {
+export default function confetti(c: Partial<ConfettiConfig> = {}): void {
     const config = Config.init(c);
     const engine = Engine.acquire();
     engine.trigger(config);
